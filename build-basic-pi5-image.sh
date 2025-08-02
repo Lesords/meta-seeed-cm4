892bc9dd59cec1c78705ac6606a3fb7c52f0916c
Lesords <lesords168@gmail.com>
2025-08-02 12:17:36 +0000

    ci: add new action

--- build-basic-pi5-image.sh
+++ build-basic-pi5-image.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-git clone -b kirkstone https://github.com/openembedded/meta-openembedded.git --depth=1
-git clone -b kirkstone git://git.yoctoproject.org/meta-raspberrypi --depth=1
+git clone -b scarthgap https://github.com/openembedded/meta-openembedded.git --depth=1
+git clone -b scarthgap git://git.yoctoproject.org/meta-raspberrypi --depth=1
 
 source oe-init-build-env # in build dir
 
-bitbake-layers add-layer ../meta-raspberrypi
+bitbake-layers add-layer ../meta-raspberrypi/
 bitbake-layers add-layer ../meta-openembedded/meta-oe/
 bitbake-layers add-layer ../meta-openembedded/meta-python/
+bitbake-layers add-layer ../meta-seeed-reterminal/
 
 # modify local.conf to build raspberrypi5 system
-sed -i '/^MACHINE/s/= .*$/= "raspberrypi5"/g' conf/local.conf
+sed -i '/^MACHINE/s/= .*$/= "seeed-recomputer-r2x"/g' conf/local.conf
 echo "ENABLE_UART = \"1\"" >> conf/local.conf
+echo "INIT_MANAGER = \"systemd\"" >> conf/local.conf
+echo "LICENSE_FLAGS_ACCEPTED = \"synaptics-killswitch\"" >> conf/local.conf
 
 # building image
 bitbake rpi-test-image
